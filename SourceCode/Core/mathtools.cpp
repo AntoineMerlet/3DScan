119234f71239d633ae1803f89ef0abd67ecc837d
gulnurungan <gulnur.ungan@gmail.com>
2018-01-05 13:21:01 +0100

    added filters needed to document

--- SourceCode/Core/mathtools.cpp
+++ SourceCode/Core/mathtools.cpp
@@ -1,5 +1,12 @@
 #include "mathtools.h"
 
 namespace Core {
-
+pcl::PointCloud<pcl::PointNormal>::Ptr normal2PointNormal(pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_in, pcl::PointCloud<pcl::Normal>::Ptr normals)
+{
+    pcl::PointCloud<pcl::PointNormal>::Ptr cloud_with_normals (new pcl::PointCloud<pcl::PointNormal>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_xyz(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::copyPointCloud(*cloud_xyz, *cloud_in);
+    pcl::concatenateFields (*cloud_xyz, *normals, *cloud_with_normals);
+    return cloud_with_normals;
+}
 }
--- SourceCode/Core/registration.h
+++ SourceCode/Core/registration.h
@@ -1,7 +1,12 @@
 #ifndef REGISTRATION_H
 #define REGISTRATION_H
 
+#include <pcl/point_cloud.h>
+#include <pcl/registration/correspondence_estimation.h>
+
 namespace Core {
+pcl::Correspondences correspKD(pcl::PointCloud<pcl::PointXYZRGB>::Ptr, pcl::PointCloud<pcl::PointXYZRGB>::Ptr, const float &);
+pcl::Correspondences correspRpoly(pcl::PointCloud<pcl::PointXYZRGB>::Ptr, pcl::PointCloud<pcl::PointXYZRGB>::Ptr, const int &);
 
 }
 
